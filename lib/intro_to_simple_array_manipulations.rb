a1920454eb3d29c5e69ec181a62cb5aba50968bc
convivial-parser-7775 <avelonpang@gmail.com>
2020-09-22 22:21:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,8 +6,7 @@ end
 
 def using_insert(list_of_programming_languages, another_language)
   #puts list_of_programming_languages[4].insert(another_language)
-  insert = list_of_programming_languages[4].insert(another_language)
-  insert
+ list_of_programming_languages = list_of_programming_languages[4].insert(another_language)
 end 
 
 def using_uniq(haircuts)
